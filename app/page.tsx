1c60df617d1b7f2795e394430fc18e43d1803fbc
Luiz Felipe <lfelipecx@gmail.com>
2024-04-30 12:00:01 -0300

    feat: add promo banner01 to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -6,12 +7,25 @@ export default function Home() {
   return (
     <main>
       <Header />
+
       <div className="px-5 pt-6">
         <Search />
       </div>
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/BannerPizza.png"
+          alt="Até 30% de desconto na pizza"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </main>
   );
