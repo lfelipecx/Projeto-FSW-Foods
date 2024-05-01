8bfc786a7c7c739eea11b4d107c67cb080ab9cf4
Luiz Felipe <lfelipecx@gmail.com>
2024-05-01 12:22:52 -0300

    feat: add product page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,49 @@
+import { notFound } from "next/navigation";
+import { db } from "../../_lib/prisma";
+import ProductImage from "./_components/product-image";
+import ProductDetails from "./_components/product-details";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProductPage = async ({ params: { id } }: ProductPageProps) => {
+  //Buscando os dados do restaurante pelo id do produto
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) {
+    return notFound;
+  }
+
+  return (
+    <div>
+      {/** IMAGEM */}
+      <ProductImage product={product} />
+
+      {/** TITULO E PREÇO  */}
+      <ProductDetails product={product} complementaryProduct={juices} />
+    </div>
+  );
+};
+
+export default ProductPage;
